e456569c7c73c2b773b56ddb17dc20b7e42a5c67
dule <d@miletich.cc>
2019-08-13 20:56:43 +0200

    Control form input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+type FormValues = { [key: string]: string };
 
 const App: React.FC = () => {
+  const [state, setState] = useState<FormValues>({});
+
   return (
     <form>
-      <input type="email" name="email" />
-      <input type="password" name="password" />
+      <input
+        type="email"
+        name="email"
+        value={state.email || ""}
+        onChange={e => {
+          e.persist();
+          setState(state => ({ ...state, [e.target.name]: e.target.value }));
+        }}
+      />
+      <input
+        type="password"
+        name="password"
+        value={state.password || ""}
+        onChange={e => {
+          e.persist();
+          setState(state => ({ ...state, [e.target.name]: e.target.value }));
+        }}
+      />
       <button type="submit">Submit</button>
     </form>
   );
