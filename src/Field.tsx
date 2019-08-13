4c8711479ad86ec38daca4ef9040066dfe2a2c94
dule <d@miletich.cc>
2019-08-13 22:03:49 +0200

    Memoize the Field component

--- src/Field.tsx
+++ src/Field.tsx
@@ -9,23 +9,27 @@ type Props = {
   setFormState: (value: React.SetStateAction<FormValues>) => void;
 };
 
-const Field: React.FC<Props> = ({ name, type, formState, setFormState }) => {
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      e.persist();
-      setFormState(state => ({ ...state, [e.target.name]: e.target.value }));
-    },
-    [formState[name]]
-  );
+const Field: React.FC<Props> = React.memo(
+  ({ name, type, formState, setFormState }) => {
+    const handleChange = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.persist();
+        setFormState(state => ({ ...state, [e.target.name]: e.target.value }));
+      },
+      [formState[name]]
+    );
 
-  return (
-    <input
-      name={name}
-      type={type}
-      value={formState[name] || ""}
-      onChange={handleChange}
-    />
-  );
-};
+    return (
+      <input
+        name={name}
+        type={type}
+        value={formState[name] || ""}
+        onChange={handleChange}
+      />
+    );
+  },
+  (prevProps, nextProps) =>
+    prevProps.formState[prevProps.name] === nextProps.formState[nextProps.name]
+);
 
 export default Field;
